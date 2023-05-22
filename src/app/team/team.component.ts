7bb4441720c3cc61e012c87df7bae39a51a3e58c
PC-150621\User <yuval.salo.95@gmai.com>
2023-05-22 16:01:28 +0300

    Finished

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+interface TeamMember{
+  name: string;
+  level: number;
+}
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent {
-  @Input() team: string[]  = []
+  @Input() team: TeamMember[]  = []
   @Input() index  = 0;
 
 
